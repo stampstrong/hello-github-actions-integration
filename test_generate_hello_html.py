7bccaa3cda0c92bd041336709178a4f6e68a5757
atbee <athibet.pi@gmail.com>
2021-08-31 15:06:07 +0700

    Use pytest :bee:

--- test_generate_hello_html.py
+++ test_generate_hello_html.py
@@ -1,15 +1,8 @@
-import unittest
-
 from generate_hello_html import generate_hello_html
 
 
-class TestGenerateHelloHTML(unittest.TestCase):
-    def test_hello_gihub_actions(self):
-        self.assertEqual(generate_hello_html(""), "<span>Hello Github Actions!</span>")
+def test_hello_gihub_actions():
+    assert generate_hello_html("") == "<span>Hello Github Actions!</span>"
 
-    def test_hello_someone(self):
-        self.assertEqual(generate_hello_html("Athibet"), "<span>Hello Athibet!</span>")
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_hello_someone():
+    assert generate_hello_html("Athibet") == "<span>Hello Athibet!</span>"
